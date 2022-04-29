12324426e9676a3cdcb3f64612b6638959e8ccc1
imjbmkz <67272807+imjbmkz@users.noreply.github.com>
2022-04-29 21:42:38 +0800

    added sql exercises

--- Exercises/SQL/sql_exercise_1.sql
+++ Exercises/SQL/sql_exercise_1.sql
@@ -1,1 +1,94 @@
+-- Use the following table to answer the following questions. Run the queries in mode.com
+SELECT * 
+  FROM tutorial.aapl_historical_stock_price
+ LIMIT 5;
 
+-- How many records are there in AAPL stock prices?
+-- Write your query below.
+
+
+
+
+
+-- What is the maximum close value?
+-- Write your query below.
+
+
+
+
+
+-- What is the total volume in month 3?
+-- Write your query below.
+
+
+
+
+
+-- What is the average high price?
+-- Write your query below.
+
+
+
+
+
+-- Which month has the largest average close price?
+-- Write your query below.
+
+
+
+
+
+-- Which month has the lowest average close price?
+-- Write your query below.
+
+
+
+
+
+-- What is APPL's closing price during your birthday in 2013? Hint: use the actual 
+-- date format in table, and enclose it inside single quotes ('').
+-- Write your query below.
+
+
+
+
+
+-- What is the average closing price of AAPL in April 2010?
+-- Write your query below.
+
+
+
+
+-- What is the very first AAPL opening price in the data?
+-- Write your query below.
+
+
+
+
+
+-- Use the following table to answer the following questions. Run the queries in mode.com
+SELECT * 
+  FROM tutorial.excel_sql_inventory_data
+ LIMIT 5;
+ 
+SELECT COUNT (*) 
+  FROM tutorial.excel_sql_inventory_data;
+
+-- With the given inventory, how much would each product type cost? Hint: get the product of inventory and wholesale.
+-- Write your query below.
+
+
+
+
+
+-- With your total cost, which product type would give you the highest profit? Hint: display the same query as in
+-- the previous problem, and create a query that will multiply inventory and price unit (total sales). 
+-- Subtract the two values.
+-- Write your query below.
+
+
+
+
+
+-- Which product type has the largest inventory?
+-- Write your query below.
